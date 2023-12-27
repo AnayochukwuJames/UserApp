511697b555a77fc8b864bf8b77bea3d1e1de9095
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-12-27 13:13:26 +0100

    update initial commit

--- src/main/java/com/example/userapp/dto/UserRequest.java
+++ src/main/java/com/example/userapp/dto/UserRequest.java
@@ -1,6 +1,9 @@
 package com.example.userapp.dto;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @Data
@@ -8,10 +11,24 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserRequest {
+//    private String firstName;
+//    private String lastName;
+//    private String address;
+//    private String phoneNumber;
+//    private String email;
+//    private String password;
+    @NotBlank(message = "first name must not be null")
+    @Size(min = 3, message = "first name must not be minimum of 3 characters")
     private String firstName;
+    @NotBlank(message = "last name must not be empty")
+    @Size(min = 3, message = "lastname must not be minimum of 3 characters")
     private String lastName;
+    @NotBlank(message = "email must be valid")
     private String address;
-    private String phoneNumber;
+    @NotBlank(message = "email must be valid")
     private String email;
+    @NotBlank(message = "phone number must be correct")
+    private String phoneNumber;
+    @NotBlank(message = "password must be strong")
     private String password;
 }
