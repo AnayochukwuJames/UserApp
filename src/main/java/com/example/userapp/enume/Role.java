78d2c22c8636390ab05b20b8356113aac634d13b
AnayochukwuJames <anayochukwujames11@gmail.com>
2024-01-16 23:59:51 +0100

    update initial commit

---
+++ src/main/java/com/example/userapp/enume/Role.java
@@ -0,0 +1,4 @@
+package com.example.userapp.enume;
+
+public enum Role {
+}
--- src/main/java/com/example/userapp/model/User.java
+++ src/main/java/com/example/userapp/model/User.java
@@ -1,12 +1,18 @@
 package com.example.userapp.model;
 
+import com.example.userapp.enume.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,7 +21,7 @@ import java.util.Date;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,6 +39,8 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    private Role role;
+
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "created_at", updatable = false)
@@ -42,4 +50,34 @@ public class User {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at")
     private Date updatedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
